8be3b4dced2583842b20eb83d5a9b6c4420ca14b
Dave Shevitz <shevitz@stripe.com>
2023-11-08 14:57:17 -0800

    initial completion of homework assignment

--- toDoList.js
+++ toDoList.js
@@ -5,10 +5,51 @@
 // If an 'Add' link is clicked, adds the item as a new list item with
 // addListItem function has been started to help you get going!
 // Make sure to add an event listener(s) to your new <li> (if needed)
+
+const todayList = document.querySelector('.today-list');
+
 const addListItem = function(e) {
   e.preventDefault();
   const input = this.parentNode.getElementsByTagName('input')[0];
   const text = input.value; // use this text to create a new <li>
+  console.log("Text", text);
 
-  // Finish function here
+ // const todayList = document.querySelector('.today-list');
+  const newItem = document.createElement('li', );
+  const newItemTitle = document.createElement('span');
+  newItemTitle.innerHTML = text;
+
+  const deleteButton = document.createElement('a');
+  deleteButton.className = 'delete';
+  deleteButton.innerText = 'Delete';
+
+  newItem.appendChild(newItemTitle);
+  newItem.appendChild(deleteButton);
+  todayList.appendChild(newItem);
+  // Clear the input
+  input.value = "";
 };
+
+
+
+const markAsDone = (e) => {
+  if (e.target.tagName = 'LI') {
+    e.target.parentNode.classList.toggle('done');
+  }
+}
+
+const addButton = document.querySelector('.add-item');
+const taskList = document.querySelector('.today-list');
+
+addButton.addEventListener('click', addListItem);
+taskList.addEventListener('click', markAsDone);
+
+// Delete the list item when Delete is clicked
+todayList.addEventListener('click', function(e) {
+  if (e.target && e.target.classList.contains('delete')) {
+    const listItem = e.target.parentElement;
+    listItem.remove();
+  }
+});
+
+
