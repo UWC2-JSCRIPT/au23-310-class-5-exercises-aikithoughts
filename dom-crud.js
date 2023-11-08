b066227d11c2f6d290d20a7cdbb381a87785ae6f
Dave Shevitz <shevitz@stripe.com>
2023-11-08 13:11:08 -0800

    update with in class work

--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,26 @@
 // Create a new <a> element containing the text "Buy Now!" 
 // with an id of "cta" after the last <p>
+const main = document.querySelector("main");
+const anchor = document.createElement("a")
+anchor.innerText = "Buy Now!";
+anchor.setAttribute("id", "cta");
+main.appendChild(anchor);
 
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
+const image = document.querySelector("img");
+console.log("data-color: ", image.getAttribute("data-color"));
 
 
 // Update the third <li> item ("Turbocharged"), 
 // set the class name to "highlight"
-
+const featureList = document.querySelector("ul");
+const turbo = featureList.getElementsByTagName("li")[2];
+turbo.className = "highlight"; // Trying a different way from setAttribute
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+const paragraphs = document.querySelectorAll("p"); // I know we only have one paragraph, but thought this was a good idea.
+const lastParagraph = paragraphs[paragraphs.length -1];
+main.removeChild(lastParagraph);
--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,15 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+const count = document.getElementById("count");
+count.innerText = 0;
+
+const plusElement = document.getElementById("plus");
+
+plusElement.addEventListener("click", () => {
+    count.innerText = parseInt(count.innerText) + 1;
+})
 // When a user clicks the – element, the count should decrease by 1 on screen.
+const minusElement = document.getElementById("minus");
+
+minusElement.addEventListener("click", () => {
+    count.innerText = parseInt(count.innerText) - 1;
+})
